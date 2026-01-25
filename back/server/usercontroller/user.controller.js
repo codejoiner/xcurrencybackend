2fe4c5a8159247e10011b3fe076d70fb299a8241
codejoiner <codejoiner15@gmail.com>
2026-01-25 10:28:28 +0200

    .........

--- back/server/usercontroller/user.controller.js
+++ back/server/usercontroller/user.controller.js
@@ -182,6 +182,7 @@ const Login = async (req, res) => {
         }
     }
   } catch (err) {
+    console.log(err)
     console.error("Error in Login Controller:", err.message);
     return res.status(500).json({ message: "Login failed Try again!" });
   }
