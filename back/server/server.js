1321b27187a2c20185757c59452f11635d3c2220
codejoiner <codejoiner15@gmail.com>
2026-01-28 16:24:40 +0200

    .............

--- back/server/server.js
+++ back/server/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
   
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://xcurrency.vercel.app",
     method:['POST','GET','DELETE','PUT'],
     credentials:true
 }))
