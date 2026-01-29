4d860ccd04824ad0d88850b3dbb491cdf9f937ef
codejoiner <codejoiner15@gmail.com>
2026-01-29 19:46:54 +0200

    .......................

--- back/server/mailconfig/transpoter.js
+++ back/server/mailconfig/transpoter.js
@@ -1,13 +1,12 @@
 const nodemailer = require('nodemailer');
-
 require("dotenv").config();
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
-        user: process.env.BREVO_USER,
-        pass: process.env.BREVO_PASS 
+        user:process.env.BREVO_USER,
+        pass:process.env.BREVO_PASS 
     }
 });
 
